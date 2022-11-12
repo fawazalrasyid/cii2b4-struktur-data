69b9b4d8ff2c8b3db8c987b31c5818432592c624
fawazalrasyid <alrasyid.fawaz16@gmail.com>
2022-11-12 22:52:27 +0700

    update tp-mod8

--- praktikum/mod-8/tp/tp-mod8/CSLL.cpp
+++ praktikum/mod-8/tp/tp-mod8/CSLL.cpp
@@ -4,43 +4,38 @@ void createList_1301213016(listLagu &L){
     first(L) = NULL;
 }
 
-void createElemen_1301213016(infotype laguBaru, adr pLagu) {
+void createElemen_1301213016(infotype laguBaru, adr &pLagu) {
     pLagu = new element;
     info(pLagu) = laguBaru;
     next(pLagu) = NULL;
 }
 
-void insertFirst_1301213016(listLagu &L, adr pLagu) {
-    if (first(L) == NULL) {
+void insertLast_1301213016(listLagu &L, adr pLagu) {
+    if (first(L) == NULL){
         first(L) = pLagu;
         next(pLagu) = pLagu;
     } else {
         adr last = first(L);
-
-        while (next(last) != first(L)) {
+        while (next(last) != first(L)){
             last = next(last);
         }
-
         next(pLagu) = first(L);
         next(last) = pLagu;
-        first(L) = pLagu;
     }
 }
 
-void deleteLast_1301213016(listLagu &L, adr pLagu) {
-    adr Q;
-    if (first(L) == NULL) {
+void deleteFirst_1301213016(listLagu &L, adr &pLagu) {
+    if (first(L) == NULL){
         cout << "List Kosong" << endl;
-    } else if (next(first(L)) == NULL) {
-        pLagu = first(L);
-        first(L) = NULL;
     } else {
-        Q = first(L);
-        while (next(next(Q)) != NULL) {
-            Q = next(Q);
+        adr last = first(L);
+        while (next(last) != first(L)){
+            last = next(last);
         }
-        pLagu = next(Q);
-        next(Q) = NULL;
+        pLagu = first(L);
+        first(L) = next(first(L));
+        next(last) = first(L);
+        next(pLagu) = NULL;
     }
 }
 
@@ -49,29 +44,15 @@ void showSemuaLagu_1301213016(listLagu L) {
         cout << "List Kosong" << endl;
     } else {
         adr pLagu = first(L);
+        int i = 1;
 
         do {
-            cout << "Artis: " << info(pLagu).artis << endl;
-            cout << "Judul: " << info(pLagu).judul << endl;
-            cout << "Genre: " << info(pLagu).genre << endl;
-            cout << "playtime: " << info(pLagu).playtime << endl;
-
-            cout << endl;
+            cout << i << ". " << info(pLagu).artis << " " << info(pLagu).judul << " " << info(pLagu).genre << " " << info(pLagu).playtime << endl;
 
             pLagu = next(pLagu);
+            i++;
 
         } while (pLagu != first(L));
-
-        /**while (pLagu != NULL && pLagu != first(L)) {
-            cout << "Artis: " << info(pLagu).artis << endl;
-            cout << "Judul: " << info(pLagu).judul << endl;
-            cout << "Genre: " << info(pLagu).genre << endl;
-            cout << "playtime: " << info(pLagu).playtime << endl;
-
-            cout << endl;
-
-            pLagu = next(pLagu);
-        }**/
 
         cout << endl;
     }
--- praktikum/mod-8/tp/tp-mod8/main.cpp
+++ praktikum/mod-8/tp/tp-mod8/main.cpp
@@ -10,37 +10,134 @@ int main()
 
     createList_1301213016(L);
 
-    int i;
-    int jumlah;
     infotype data;
 
-    i = 0;
+    data.artis = "LE SSERAFIM";
+    data.judul = "Good Parst";
+    data.genre = "Kpop";
+    data.playtime = 78;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
 
-    cout << "Jumlah data yang akan ditambahkan: ";
-    cin >> jumlah;
-    cout << endl;
+    data.artis = "The Weekend";
+    data.judul = "After Hours";
+    data.genre = "Pop";
+    data.playtime = 61;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
 
-    while (i < jumlah) {
-        cout << "Artis: ";
-        cin >> data.artis;
+    data.artis = "LE SSERAFIM";
+    data.judul = "ANTIFRAGILE";
+    data.genre = "Kpop";
+    data.playtime = 60;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
 
-        cout << "Judul: ";
-        cin >> data.judul;
+    data.artis = "DAY6";
+    data.judul = "Congratulation";
+    data.genre = "Korean Rock";
+    data.playtime = 58;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
 
-        cout << "Genre: ";
-        cin >> data.genre;
+    data.artis = "NewJeans";
+    data.judul = "Attention";
+    data.genre = "Kpop";
+    data.playtime = 57;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
 
-        cout << "Playtime: ";
-        cin >> data.playtime;
+    data.artis = "Juicy Luicy";
+    data.judul = "Mawar Jingga";
+    data.genre = "Pop";
+    data.playtime = 43;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
 
-        cout << endl;
+    data.artis = "Hivi!";
+    data.judul = "Remaja";
+    data.genre = "Pop";
+    data.playtime = 38;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
 
-        createElemen_1301213016(data, pLagu);
-        insertFirst_1301213016(L, pLagu);
+    data.artis = "NewJeans";
+    data.judul = "Hype Boy";
+    data.genre = "Kpop";
+    data.playtime = 37;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
 
-        i++;
-    }
+    data.artis = "Sheila On 7";
+    data.judul = "Dan...";
+    data.genre = "Pop";
+    data.playtime = 36;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
 
+    data.artis = "Tulus";
+    data.judul = "Sewindu";
+    data.genre = "Pop";
+    data.playtime = 34;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
+
+    data.artis = "Tulus";
+    data.judul = "Labirin";
+    data.genre = "Pop";
+    data.playtime = 32;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
+
+    data.artis = "NMIXX";
+    data.judul = "DICE";
+    data.genre = "Kpop";
+    data.playtime = 30;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
+
+    data.artis = "Juicy Luicy";
+    data.judul = "Lantas";
+    data.genre = "Pop";
+    data.playtime = 29;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
+
+    data.artis = "LE SSERAFIM";
+    data.judul = "Blue Flame";
+    data.genre = "Kpop";
+    data.playtime = 29;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
+
+    data.artis = "Yovie Widianto, Tulus, Glenn Fredly";
+    data.judul = "Adu Rayu";
+    data.genre = "Pop";
+    data.playtime = 27;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
+
+    data.artis = "Utha Likumahuwa";
+    data.judul = "Sesaat Kau Hadir";
+    data.genre = "Jazz";
+    data.playtime = 24;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
+
+    data.artis = "DPR LIVE";
+    data.judul = "Jasmine";
+    data.genre = "Hip-hop";
+    data.playtime = 21;
+    createElemen_1301213016(data, pLagu);
+    insertLast_1301213016(L, pLagu);
+
+
+    cout << "menampilkan semua element list" << endl;
+    showSemuaLagu_1301213016(L);
+
+    deleteFirst_1301213016(L, pLagu);
+
+    cout << "menampilkan semua element list setelah element pertama dihapus" << endl;
     showSemuaLagu_1301213016(L);
 
     return 0;
